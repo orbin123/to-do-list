028f71d5bb40f48b253bcae592fb83af5514220e
unknown <orbinsunny@gmail.com>
2022-08-12 18:32:25 +0530

    first commit

---
+++ date.js
@@ -0,0 +1,15 @@
+//jshint esversion:6
+
+exports.getDate = function getDate(){
+let today = new Date();
+
+let options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long"
+};
+
+return today.toLocaleDateString("en-US", options);
+
+
+}
